675a1f3c1f8f77efdf45bb04271b85a4029b0a0c
7noptic <7noptic@gmail.com>
2021-02-13 14:21:58 +0300

    slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -341,6 +341,23 @@ window.addEventListener('DOMContentLoaded', () => {
             },
 
         });
+        var galleryThumbs = new Swiper('.gallery-thumbs', {
+            spaceBetween: 10,
+            slidesPerView: 4,
+            freeMode: true,
+            watchSlidesVisibility: true,
+            watchSlidesProgress: true,
+          });
+          var galleryTop = new Swiper('.gallery-top', {
+            spaceBetween: 10,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+            thumbs: {
+              swiper: galleryThumbs
+            }
+          });
         /* CATALOG MEDIA LIST */
         const catalogParent = document.querySelector('.catalog__sidebar');
 
