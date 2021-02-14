ce79ee0ca9e865a12d490578fef6c5f3329f3b14
7noptic <7noptic@gmail.com>
2021-02-14 18:10:50 +0300

    pravki

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,68 +7,7 @@ import GLightbox from 'glightbox';
 Swiper.use([Navigation, Pagination, Thumbs]);
 window.addEventListener('DOMContentLoaded', () => {
         const lightbox = GLightbox({});
-        $('.select').each(function () {
-            // Variables
-            var $this = $(this),
-                selectOption = $this.find('option'),
-                selectOptionLength = selectOption.length,
-                selectedOption = selectOption.filter(':selected'),
-                dur = 500;
 
-            $this.hide();
-            // Wrap all in select box
-            $this.wrap('<div class="select"></div>');
-            // Style box
-            $('<div>', {
-                class: 'select__gap js-required',
-                text: 'Не выбрано'
-            }).insertAfter($this);
-
-            var selectGap = $this.next('.select__gap.js-required'),
-                caret = selectGap.find('.caret');
-            // Add ul list
-            $('<ul>', {
-                class: 'select__list'
-            }).insertAfter(selectGap);
-
-            var selectList = selectGap.next('.select__list');
-            // Add li - option items
-            for (var i = 0; i < selectOptionLength; i++) {
-                $('<li>', {
-                    class: 'select__item',
-                    html: $('<span>', {
-                        text: selectOption.eq(i).text()
-                    })
-                })
-                    .attr('data-value', selectOption.eq(i).val())
-                    .appendTo(selectList);
-            }
-            // Find all items
-            var selectItem = selectList.find('li');
-
-            selectList.slideUp(0);
-            selectGap.on('click', function () {
-                if (!$(this).hasClass('on')) {
-                    $(this).addClass('on');
-                    selectList.slideDown(dur);
-
-                    selectItem.on('click', function () {
-                        var chooseItem = $(this).data('value');
-
-                        $('select').val(chooseItem).attr('selected', 'selected');
-                        selectGap.text($(this).find('span').text());
-
-                        selectList.slideUp(dur);
-                        selectGap.removeClass('on');
-                    });
-
-                } else {
-                    $(this).removeClass('on');
-                    selectList.slideUp(dur);
-                }
-            });
-
-        });
         $('[type="tel"]').mask("+7 (999) 999-99-99");
         /* BURGER-MENU */
         let headerMenu = document.querySelector('.header__nav'),
@@ -255,6 +194,37 @@ window.addEventListener('DOMContentLoaded', () => {
                     slidesPerView: 6,
                     spaceBetween: 40
                 },
+
+
+            }
+
+        });
+        let swiperProd = new Swiper('.swiper-container-prod', {
+            slidesPerView: 3,
+            spaceBetween: 20,
+            //Инициализация в табах
+            observer: true,
+            observeParents: true,
+            navigation: {
+                nextEl: '.prod-next',
+                prevEl: '.prod-prev',
+            },
+            // Responsive breakpoints
+            breakpoints: {
+
+                // when window width is <= 320px
+                0: {
+                    slidesPerView: 1,
+                },
+                575: {
+                    slidesPerView: 2,
+                },
+                767: {
+                    slidesPerView: 3,
+                },
+
+
+
 
 
             }
